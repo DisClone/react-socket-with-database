5eec0f1993e6b199d1a7a3bc9d07735fc0981579
preston <mechampreston@gmail.com>
2016-09-05 17:54:40 -0600

    Fixed a foreign key reference, added ;'s

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,25 @@
+create table users (
+  id serial primary key,
+  username varchar(50),
+  password varchar(300)
+);
+
+create table groups (
+  id serial primary key
+);
+
+create table groups_users_relations (
+  id serial primary key,
+  user_id integer references users(id),
+  group_id integer references groups(id)
+);
+
+create table test_msgs (
+  id serial primary key,
+  body varchar(300),
+  author varchar(40)
+);
+
 create table messages (
   id serial primary key,
   message_time varchar(40),
@@ -5,27 +27,4 @@ create table messages (
   is_group_message boolean,
   recipient_id integer references users(id),
   group_recipient integer references groups(id)
-)
-
-create table users (
-  id serial primary key,
-  username varchar(50),
-  password varchar(300)
-
-)
-
-create table groups (
-  id serial primary key,
-)
-
-create table groups_users_relations (
-  id serial primary key,
-  user_id integer references users(id),
-  group_id integer references users(id)
-)
-
-create table test_msgs (
-  id serial primary key,
-  body varchar(300),
-  author varchar(40)
-)
+);
